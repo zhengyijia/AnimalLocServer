ce97920f58101b0eac0595e09dd6588197933d40
zhengyijia <zhengyijia142@qq.com>
2017-11-30 21:07:32 +0800

    Fix some bugs.

--- src/main/java/model/LoginModel.java
+++ src/main/java/model/LoginModel.java
@@ -3,6 +3,7 @@ package model;
 import contract.LoginContract;
 import entity.TableBean.AnimalLocImeiEntity;
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
@@ -13,16 +14,27 @@ public class LoginModel implements LoginContract.ILoginModel {
     // 验证是否存在对应IMEI
     @Override
     public boolean verifyIMEI(String IMEI) {
+        boolean verify = false;
+
         // 打开线程安全的Session对象
-        Session session = HibernateUtil.currentSession();
-        // 打开事务
-        Transaction transaction = session.beginTransaction();
-        Criteria c = session.createCriteria(AnimalLocImeiEntity.class);
-        c.add(Restrictions.eq("imei", IMEI));
-        boolean verify = !c.list().isEmpty();
-        transaction.commit();
-        //通过工具类关闭Session
-        HibernateUtil.closeSession();
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.currentSession();
+            // 打开事务
+            transaction = session.beginTransaction();
+            Criteria c = session.createCriteria(AnimalLocImeiEntity.class);
+            c.add(Restrictions.eq("imei", IMEI));
+            verify = !c.list().isEmpty();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            //通过工具类关闭Session
+            HibernateUtil.closeSession();
+        }
 
         return verify;
     }
