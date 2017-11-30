11f16d62761d272b7a1bf233d9158ea3d260a749
zhengyijia <zhengyijia142@qq.com>
2017-11-30 21:12:11 +0800

    Fix some bugs.

--- src/main/java/model/LocationModel.java
+++ src/main/java/model/LocationModel.java
@@ -2,6 +2,7 @@ package model;
 
 import contract.LocationContract;
 import entity.TableBean.AnimalLocLocInfoEntity;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import util.HibernateUtil;
@@ -11,14 +12,25 @@ public class LocationModel implements LocationContract.ILocationModel {
     // 保存gps数据
     @Override
     public void saveLocInfo (AnimalLocLocInfoEntity locInfo) {
+
         // 打开线程安全的Session对象
-        Session session = HibernateUtil.currentSession();
-        // 打开事务
-        Transaction transaction = session.beginTransaction();
-        session.save(locInfo);
-        transaction.commit();
-        //通过工具类关闭Session
-        HibernateUtil.closeSession();
+        Transaction transaction = null;
+        try {
+            // 打开线程安全的Session对象
+            Session session = HibernateUtil.currentSession();
+            // 打开事务
+            transaction = session.beginTransaction();
+            session.save(locInfo);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            // 通过工具类关闭Session
+            HibernateUtil.closeSession();
+        }
     }
 
 }
